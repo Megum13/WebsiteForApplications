fa87e6bfbb0df438b49ecb4fdc0065312e3f1fe9
Megum13 <88092386+Megum13@users.noreply.github.com>
2022-03-24 00:57:14 +0300

    Add files via upload

--- Site/reg.php
+++ Site/reg.php
@@ -1,3 +1,13 @@
 <?php
-  echo 12;
+$mysqli = new mysqli("localhost", "root", "pass", "testbase");
+
+$fname = htmlspecialchars($_POST['firstname']);
+$lname = htmlspecialchars($_POST['lastname']);
+$login = htmlspecialchars($_POST['login']);
+$pass = htmlspecialchars($_POST['password']);
+
+echo "Вы зарегестрированы как $login <br> <a href='/Site/authorization.html'>Войти</a>";
+
+$mysqli->query("INSERT INTO users VALUES ('$fname', '$lname', '$login', '$pass')");
+
 ?>
